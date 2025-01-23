b83dedd5ef1307f6c0f3d3a40fc683a0f2985e37
alishersv <alishersv@gmail.com>
2025-01-22 22:55:44 -0500

    added server.py

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -6,5 +6,8 @@ def sentiment_analyzer(text_to_analyse):  # Define a function named sentiment_an
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    formatted_response = json.loads(response.text)
-    return {'score': formatted_response['documentSentiment']['score'],'label': formatted_response['documentSentiment']['label']}  # Return the response text from the API
+    if response.status_code == 200:
+        formatted_response = json.loads(response.text)
+        return {'score': formatted_response['documentSentiment']['score'],'label': formatted_response['documentSentiment']['label']}  # Return the response text from the API
+    elif response.status_code == 500:
+        return {'score': "None",'label': "None"}
--- server.py
+++ server.py
@@ -5,7 +5,11 @@
 # Import Flask, render_template, request from the flask pramework package : TODO
 # Import the sentiment_analyzer function from the package created: TODO
 
+from flask import Flask, request, render_template
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
 #Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -15,6 +19,12 @@ def sent_analyzer():
         score for the provided text.
     '''
     # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = sentiment_analyzer(text_to_analyze)
+
+    return f"The given text has been identified as {response['label']} with a score of {response['score']}."
+
 
 @app.route("/")
 def render_index_page():
@@ -22,7 +32,9 @@ def render_index_page():
         page over the Flask channel
     '''
     #TODO
+    return render_template('index.html')
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
     '''#TODO
+    app.run(host="0.0.0.0", port=5000)
